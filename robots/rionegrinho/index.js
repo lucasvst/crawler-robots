3dad7652fa5862fc8c53f91fd007546678032733
Lucas Teles <lucas.teles@fluig.com>
2018-01-03 10:06:07 -0200

    rionegrinho robot

---
+++ robots/rionegrinho/index.js
@@ -0,0 +1,20 @@
+const run = require('./../../core/index');
+
+run('http://www.rionegrinho.sc.gov.br/licitacao', $ => {
+
+    const rows = [];
+
+    const items = $('ul#bidding_list li');
+
+    items.each((i, item) => {
+
+        rows.push({
+            title: $('.bidding_title', item).text(),
+            opening: $('.bidding_opening', item).text(),
+            object: $('.bidding_object', item).text(),
+        });
+    });
+
+    return rows;
+
+});
