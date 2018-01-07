18847656367ea2749815d3883db2272358780bb7
Lucas Teles <lucas.teles@fluig.com>
2018-01-07 16:16:39 -0200

    robot updated

--- app/robots/rionegrinho/index.js
+++ app/robots/rionegrinho/index.js
@@ -1,19 +1,51 @@
+const Schema = require('./../../core/persistence/schema');
+
 const run = require('./../../core/engine');
+
+const AGENCY_NAME = 'PREFEITURA DE RIO NEGRINHO/SC';
+
+const modalityNumberPattern = /^([A-zÀ-ÿ\s-]*)\sNº\s([\d\/]*)/i;
+const startsInPattern = /^\w*:\s([\d/\s:-]*)/i;
+
+const extractModality = str => {
+    if (!str) { return; }
+    const match = str.match(modalityNumberPattern);
+    return match[1];
+}
+
+const extractNumber = str => {
+    if (!str) { return; }
+    const match = str.match(modalityNumberPattern);
+    return match[2];
+}
+
+const extractObjectDescription = str => str;
+
+const extractStartsInRaw = str => {
+    if (!str) { return; }
+    const match = str.match(startsInPattern);
+    return match[1];
+}
 
 run('http://www.rionegrinho.sc.gov.br/licitacao', $ => {
 
     const rows = [];
 
-    // const items = $('ul#bidding_list li');
+    const items = $('ul#bidding_list li');
 
-    // items.each((i, item) => {
+    items.each((i, item) => {
 
-    //     rows.push({
-    //         title: $('.bidding_title', item).text(),
-    //         opening: $('.bidding_opening', item).text(),
-    //         object: $('.bidding_object', item).text(),
-    //     });
-    // });
+        const obj = {
+            number: extractNumber($('.bidding_title', item).text()),
+            modality: extractModality($('.bidding_title', item).text()),
+            agency: AGENCY_NAME,
+            object_description: extractObjectDescription($('bidding_object', item).text()),
+            starts_in_raw: extractStartsInRaw($('.bidding_opening', item).text()),
+            // edital: '',
+        }
+
+        rows.push(new Schema(obj));
+    });
 
     return rows;
 
